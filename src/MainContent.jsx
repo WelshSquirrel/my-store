f9128bb1154541cd5393c6e85a053b4d08541b60
WelshSquirrel <109298669+WelshSquirrel@users.noreply.github.com>
2023-03-01 16:44:08 +0000

    fix

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,7 +1,28 @@
 import React, { Component } from "react";
 
 export default class MainContent extends Component {
+  state = { pageTitle: "Customers", customersCount: 5 };
+
   render() {
-    return <div> Hello Main</div>;
+    return (
+      <div>
+        <h4 className="border-bottom m-1 p-1">
+          {" "}
+          {this.state.pageTitle}
+          <span className="badge badge-dark m-2">
+            {this.state.customersCount}
+          </span>
+          <button className="btn btn=info" onClick={this.onRefreshClick}>
+            Refresh
+          </button>
+        </h4>
+      </div>
+    );
   }
+
+  onRefreshClick = () => {
+    this.setState({
+      customersCount: 7,
+    });
+  };
 }
