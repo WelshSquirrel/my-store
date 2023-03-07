822aae19d0c5007dfc81445616720d9b7d30fe69
WelshSquirrel <109298669+WelshSquirrel@users.noreply.github.com>
2023-03-07 17:50:21 +0000

    Photo and refactor

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -5,21 +5,50 @@ export default class MainContent extends Component {
     pageTitle: "Customers",
     customersCount: 5,
     customers: [
-      { id: 1, name: "Ben", phone: "123-123" },
-      { id: 2, name: "Steve", phone: "123-123" },
-      { id: 3, name: "James", phone: "123-123" },
-      { id: 4, name: "Mark", phone: "123-123" },
-      { id: 5, name: "John", phone: "123-123" },
+      {
+        id: 1,
+        name: "Ben",
+        phone: "673-825",
+        address: { city: "London" },
+        photo: "https://picsum.photos/id/200/60",
+      },
+      {
+        id: 2,
+        name: "Steve",
+        phone: "254-857",
+        address: { city: "Manchester" },
+        photo: "https://picsum.photos/id/234/60",
+      },
+      {
+        id: 3,
+        name: "James",
+        phone: "473-893",
+        address: { city: "London" },
+        photo: "https://picsum.photos/id/894/60",
+      },
+      {
+        id: 4,
+        name: "Mark",
+        phone: null,
+        address: { city: "Liverpool" },
+        photo: "https://picsum.photos/id/534/60",
+      },
+      {
+        id: 5,
+        name: "John",
+        phone: "123-433",
+        address: { city: "London" },
+        photo: "https://picsum.photos/id/256/60",
+      },
     ],
   };
 
   render() {
     return (
       <div>
-        <h4 className="border-bottom m-1 p-1">
-          {" "}
+        <h4 className="m-1 p-1">
           {this.state.pageTitle}
-          <span className="badge badge-dark m-2">
+          <span className="badge badge-secondary m-2">
             {this.state.customersCount}
           </span>
           <button className="btn btn=info" onClick={this.onRefreshClick}>
@@ -28,13 +57,16 @@ export default class MainContent extends Component {
         </h4>
 
         <table className="table">
-          <thread>
+          <thead>
             <tr>
               <th>#</th>
+              <th>Photo</th>
               <th>Customer Name</th>
               <th>Phone</th>
+              <th>City</th>
             </tr>
-          </thread>
+          </thead>
+          <tbody>{this.getCustomerRow()}</tbody>
         </table>
       </div>
     );
@@ -43,6 +75,35 @@ export default class MainContent extends Component {
   onRefreshClick = () => {
     this.setState({
       customersCount: 7,
+    });
+  };
+
+  getPhoneToRender = (phone) => {
+    if (phone) return phone;
+    else {
+      return <div className="bg-warning p-2 text-center">No Number</div>;
+    }
+  };
+
+  getCustomerRow = () => {
+    return this.state.customers.map((customer) => {
+      return (
+        <tr key={customer.id}>
+          <td>{customer.id}</td>
+          <td>
+            <img src={customer.photo} alt="Customer" />
+          </td>
+          <td
+            style={{
+              backgroundColor: customer.name.startsWith("M") ? "green" : "red",
+            }}
+          >
+            {customer.name}
+          </td>
+          <td>{this.getPhoneToRender(customer.phone)} </td>
+          <td>{customer.address.city}</td>
+        </tr>
+      );
     });
   };
 }
